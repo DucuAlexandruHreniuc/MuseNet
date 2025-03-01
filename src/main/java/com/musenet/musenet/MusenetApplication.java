8ca5ca26915a7ec252ab2a0ac7624294c1d9eb36
Ducu Hreniuc <bogdan.hreniuc@telios.ro>
2025-03-01 20:53:25 +0200

    first commit

---
+++ src/main/java/com/musenet/musenet/MusenetApplication.java
@@ -0,0 +1,13 @@
+package com.musenet.musenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusenetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musenet/musenet/MusenetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musenet.musenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusenetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
